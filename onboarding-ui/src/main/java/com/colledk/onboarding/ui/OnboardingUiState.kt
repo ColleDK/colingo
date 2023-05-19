291dc00cded3bfa0f9d629cb9efb699a17270cf9
Colle <williamdk@live.dk>
2023-05-19 15:11:08 +0200

    Added dependency

--- onboarding-ui/src/main/java/com/colledk/onboarding/ui/OnboardingUiState.kt
+++ onboarding-ui/src/main/java/com/colledk/onboarding/ui/OnboardingUiState.kt
@@ -3,5 +3,5 @@ package com.colledk.onboarding.ui
 import com.colledk.country.domain.model.Country
 
 data class OnboardingUiState(
-    val countries: List<com.colledk.country.domain.model.Country> = listOf()
+    val countries: List<Country> = listOf()
 )
