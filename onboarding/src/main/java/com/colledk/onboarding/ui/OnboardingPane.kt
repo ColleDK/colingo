f50f4a4f6e94ed60d541e6ca0137287c14c0b36c
Colle <williamdk@live.dk>
2024-06-14 17:15:02 +0200

    Finished updates on login screen

--- onboarding/src/main/java/com/colledk/onboarding/ui/OnboardingPane.kt
+++ onboarding/src/main/java/com/colledk/onboarding/ui/OnboardingPane.kt
@@ -3,7 +3,6 @@ package com.colledk.onboarding.ui
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,36 +12,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.AnimatedPane
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldDefaults
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
 import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
 import com.colledk.onboarding.R
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
@@ -69,29 +61,28 @@ fun OnboardingPane(modifier: Modifier = Modifier, onFinishOnboarding: () -> Unit
         value = navigator.scaffoldValue,
         listPane = {
             AnimatedPane {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    Text(text = "This is the main screen")
-                    Button(
-                        onClick = {
-                            navigator.navigateTo(
-                                ListDetailPaneScaffoldRole.Detail,
-                                OnboardingDestination.LOG_IN
-                            )
-                        }
-                    ) {
-                        Text(text = "Finish onboarding")
-                    }
-                }
+                OnboardingContent(
+                    onGoToLogin = {
+                        navigator.navigateTo(
+                            ListDetailPaneScaffoldRole.Detail,
+                            OnboardingDestination.LOG_IN
+                        )
+                    },
+                    onGoToSignup = {
+                        navigator.navigateTo(
+                            ListDetailPaneScaffoldRole.Detail,
+                            OnboardingDestination.SIGN_UP
+                        )
+                    },
+                    onGoToGoogle = { /*TODO*/ },
+                    onGoToFacebook = { /*TODO*/ })
             }
         },
         detailPane = {
             AnimatedPane {
                 navigator.currentDestination?.content?.let {
                     if (it == OnboardingDestination.LOG_IN) {
-                        LoginPane()
+                        LoginPane({}, { _, _ -> }, {})
                     } else {
                         // TODO
                     }
@@ -102,108 +93,104 @@ fun OnboardingPane(modifier: Modifier = Modifier, onFinishOnboarding: () -> Unit
 }
 
 @Composable
-private fun LoginPane(modifier: Modifier = Modifier) {
-    var email by remember {
-        mutableStateOf("")
-    }
-
-    var password by remember {
-        mutableStateOf("")
-    }
-
+private fun OnboardingContent(
+    onGoToLogin: () -> Unit,
+    onGoToSignup: () -> Unit,
+    onGoToGoogle: () -> Unit,
+    onGoToFacebook: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = modifier.fillMaxSize().padding(24.dp),
-        verticalArrangement = Arrangement.Bottom,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Spacer(modifier = Modifier.weight(1f))
         Image(
             painter = painterResource(id = R.drawable.world),
             contentDescription = null // Not needed for talkback
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
-            text = stringResource(id = R.string.login_title),
-            style = MaterialTheme.typography.displaySmall
+            text = stringResource(id = R.string.onboarding_app_title),
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
         )
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(
-            text = stringResource(id = R.string.login_subtitle),
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            placeholder = {
-                Text(
-                    text = stringResource(id = R.string.login_email_hint),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            },
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.email),
-                    contentDescription = null,
-                    modifier = Modifier.size(24.dp)
-                )
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            modifier = Modifier.fillMaxWidth(.8f)
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            placeholder = {
-                Text(
-                    text = stringResource(id = R.string.login_password_hint),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            },
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.password),
-                    contentDescription = null,
-                    modifier = Modifier.size(24.dp)
-                )
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth(.8f)
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = stringResource(id = R.string.login_forgot_password),
-            style = MaterialTheme.typography.labelMedium,
+        Spacer(modifier = Modifier.height(44.dp))
+        OnboardingButton(
+            btnText = stringResource(id = R.string.onboarding_login_btn),
             modifier = Modifier.fillMaxWidth(.8f),
-            textAlign = TextAlign.End
+            onClick = onGoToLogin
         )
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = { /*TODO*/ },
-            enabled = password.isNotBlank() && email.isNotBlank(),
-            modifier = Modifier.fillMaxWidth(.5f).heightIn(min = 48.dp)
+        Spacer(modifier = Modifier.height(12.dp))
+        OnboardingButton(
+            btnText = stringResource(id = R.string.onboarding_signup_btn),
+            modifier = Modifier.fillMaxWidth(.8f),
+            onClick = onGoToSignup
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(.8f),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Spacer(
+                modifier = Modifier
+                    .height(1.dp)
+                    .weight(1f)
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer)
+            )
             Text(
-                text = stringResource(id = R.string.login_login_btn),
-                style = MaterialTheme.typography.bodyMedium
+                text = stringResource(id = R.string.onboarding_other_title),
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.labelMedium
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(1.dp)
+                    .weight(1f)
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer)
             )
         }
-        Spacer(modifier = Modifier.height(100.dp))
-        Row {
-            Text(
-                text = stringResource(id = R.string.login_go_to_signup_1),
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = stringResource(id = R.string.login_go_to_signup_2),
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.Bold,
-                textDecoration = TextDecoration.Underline,
-                modifier = Modifier.clickable {
-                    // TODO
-                }
-            )
-        }
+        Spacer(modifier = Modifier.height(12.dp))
+        OnboardingButton(
+            btnText = stringResource(id = R.string.onboarding_google_btn),
+            modifier = Modifier.fillMaxWidth(.8f),
+            onClick = onGoToGoogle
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        OnboardingButton(
+            btnText = stringResource(id = R.string.onboarding_facebook_btn),
+            modifier = Modifier.fillMaxWidth(.8f),
+            onClick = onGoToFacebook
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.onboarding_guest_btn),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            modifier = Modifier.alpha(.7f)
+        )
+    }
+}
+
+@Composable
+private fun OnboardingButton(
+    btnText: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(size = 8.dp)
+    ) {
+        Text(
+            text = btnText,
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
 }
